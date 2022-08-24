9d652e0b2dfd0efb2192967ed1847d85ae24168a
Damar Wicaksono <damar.wicaksono@outlook.com>
2022-08-24 16:06:28 +0200

    feat #10: Add an implementation of MultivariateInput.
    - MultivariateInput class is defined to hold information of
      a multivariate (multi-dimensional) probabilistic input.
      It is part of the core.
    - The test suite has been expanded to test the instances
      of the class.

--- src/uqtestfuns/core/prob_input/__init__.py
+++ src/uqtestfuns/core/prob_input/__init__.py
@@ -4,5 +4,7 @@ Sub-package to model inputs probabilistically.
 __all__ = []
 
 from .univariate_input import UnivariateInput
+from .multivariate_input import MultivariateInput
 
 __all__ += univariate_input.__all__
+__all__ += multivariate_input.__all__
