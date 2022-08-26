3d66c0383cd8722249541d8515672f05a4d6d75e
Damar Wicaksono <damar.wicaksono@outlook.com>
2022-08-26 16:34:37 +0200

    Update the default input of wing weight.

--- src/uqtestfuns/test_functions/wing_weight.py
+++ src/uqtestfuns/test_functions/wing_weight.py
@@ -1,5 +1,15 @@
 """
 Module with an implementation of Wing Weight test function.
+
+The Wing Weight test function[1] is a 10-dimensional scalar-valued function
+that models a light aircraft wing.
+
+References
+----------
+
+[1] A. I. J. Forrester, A. Sóbester, and A. J. Keane, Engineering Design
+    via Surrogate Modelling: A Practical Guide, 1st ed. Wiley, 2008.
+    doi: 10.1002/9780470770801.
 """
 import numpy as np
 
@@ -15,61 +25,84 @@ DEFAULT_INPUT_DICTS = [
     {
         "name": "Sw",
         "distribution": "uniform",
-        "parameters": [150, 200]
+        "parameters": [150, 200],
+        "description": "wing area [ft^2]"
     },
     {
         "name": "Wfw",
         "distribution": "uniform",
-        "parameters": [220, 300]
+        "parameters": [220, 300],
+        "description": "weight of fuel in the wing [lb]"
     },
     {
         "name": "A",
         "distribution": "uniform",
-        "parameters": [6, 10]
+        "parameters": [6, 10],
+        "description": "aspect ratio [-]"
     },
     {
         "name": "Lambda",
         "distribution": "uniform",
-        "parameters": [-10, 10]
+        "parameters": [-10, 10],
+        "description": "quarter-chord sweep [degrees]"
     },
     {
         "name": "q",
         "distribution": "uniform",
-        "parameters": [16, 45]
+        "parameters": [16, 45],
+        "description": "dynamic pressure at cruise [lb/ft^2]"
     },
     {
         "name": "lambda",
         "distribution": "uniform",
-        "parameters": [0.5, 1.0]
+        "parameters": [0.5, 1.0],
+        "description": "taper ratio [-]"
     },
     {
         "name": "tc",
         "distribution": "uniform",
-        "parameters": [0.08, 0.18]
+        "parameters": [0.08, 0.18],
+        "description": "aerofoil thickness to chord ratio [-]"
     },
     {
         "name": "Nz",
         "distribution": "uniform",
-        "parameters": [2.5, 6.0]
+        "parameters": [2.5, 6.0],
+        "description": "ultimate load factor [-]"
     },
     {
         "name": "Wdg",
         "distribution": "uniform",
-        "parameters": [1700, 2500]
+        "parameters": [1700, 2500],
+        "description": "flight design gross weight [lb]"
     },
     {
         "name": "Wp",
         "distribution": "uniform",
-        "parameters": [0.025, 0.08]
+        "parameters": [0.025, 0.08],
+        "description": "paint weight [lb/ft^2]"
     }
 ]
 
 DEFAULT_INPUT = MultivariateInput(DEFAULT_INPUT_DICTS)
-DEFAULT_PARAMETERS = None
 
 
 class WingWeight(UQTestFun):
-    """Implementation of the Wing Weight test function."""
+    """Implementation of the Wing Weight test function.
+
+    Parameters
+    ----------
+    input : MultivariateInput
+        MultivariateInput instance that specifies 10-dimensional probabilistic
+        input model for the Wing Weight function.
+
+    Attributes
+    ----------
+    input : MultivariateInput
+        Probabilistic input of the test function.
+    spatial_dimension : int
+        Number of input variables of the test function.
+    """
 
     def __init__(self, input: MultivariateInput = DEFAULT_INPUT):
         if not isinstance(input, MultivariateInput):
@@ -81,10 +114,16 @@ class WingWeight(UQTestFun):
 
         self._spatial_dimension = DEFAULT_INPUT.spatial_dimension
         self.input = DEFAULT_INPUT
-        self.parameters = DEFAULT_PARAMETERS
 
-    def evaluate(self, xx: np.ndarray):
-        """Evaluate the Wing Weight function on a set of input values."""
+    def evaluate(self, xx: np.ndarray) -> np.ndarray:
+        """Evaluate the Wing Weight function on a set of input values.
+
+        Parameters
+        ----------
+        xx : np.ndarray
+            10-Dimensional input values given by N-by-10 arrays where
+            N is the number of input values.
+        """
         if xx.shape[1] != self.spatial_dimension:
             raise ValueError(
                 f"Wrong dimensionality of the input array!"
