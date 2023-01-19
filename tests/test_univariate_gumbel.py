3637ae6f9fbb8267c4db7c386e726bdffe0146af
Damar Wicaksono <damar.wicaksono@outlook.com>
2023-01-19 15:47:36 +0100

    Fix testing issue.

--- tests/test_univariate_gumbel.py
+++ tests/test_univariate_gumbel.py
@@ -107,7 +107,7 @@ def test_estimate_variance() -> None:
     )
 
     # Generate a sample
-    sample_size = 100000  # Should give 1e-2 accuracy
+    sample_size = 1000000  # Should give 1e-2 accuracy
     xx = my_univariate_input.get_sample(sample_size)
 
     # Estimated result
