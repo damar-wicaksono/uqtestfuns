281fa8c56d70609ea837a32a5b97e971de648148
Damar Wicaksono <damar.wicaksono@outlook.com>
2023-01-25 11:49:21 +0100

    Add an implementation of the sulfur model.

---
+++ src/uqtestfuns/test_functions/sulfur.py
@@ -0,0 +1,183 @@
+"""
+Module with an implementation of the Sulfur model test function.
+
+The Sulfur model from [1] is a nine-dimensional scalar-valued function
+that analytically computes the direct radiative forcing by sulfate aerosols.
+The test function was used in [2] for metamodeling and uncertainty propagation.
+The probabilistic input specification used in [2] was originally based
+from [3].
+
+Due to the construction of the test function, the geometric mean and geometric
+standard deviation of the response are analytically available given the input
+specification (from [2], all are lognormals). Note that the geometric mean
+and the geometric standard deviation of a lognormal distribution are
+the exponentiated mu and sigma parameters (i.e., the mean and
+standard deviation of the underlying normal distribution).
+
+To complete the test function specification, two additional parameters are
+taken from the literature: the solar constant [4] and the surface area
+of the earth [5].
+
+Notes
+-----
+
+- The equation of the Sulfur model in [2] (Eq. (12)) is erroneous.
+  The solar constant term S0 should not be squared otherwise the dimension
+  will not agree. Moreover, the equation is missing a factor of 365 in the
+  denominator because the parameter L (Sulfate lifetime in the atmosphere)
+  is given in [days] while the parameter Q (Global input flux of anthropogenic
+  sulfur is given in [TgS/year].
+- The input specification of the model here are taken from [3] (Table 2).
+  While they are similar, the parameters T and (1-Rs) in [3] are in the
+  squared form (i.e., T^2 and (1-Rs)^2).
+
+References
+----------
+[1] R. J. Charlson, S. E. Schwartz, J. M. Hales, R. D. Cess, J. A. Coakley,
+    J. E. Hansen, and D. J. Hofmann, “Climate forcing by anthropogenic
+    aerosols,” Science, vol. 255, no. 5043, pp. 423–430, 1992.
+    DOI: 10.1126/science.255.5043.423.
+[2] M. A. Tatang, W. Pan, R. G. Prinn, and G. J. McRae, “An efficient method
+    for parametric uncertainty analysis of numerical geophysical models,”
+    Journal of Geophysical Research, vol. 102, no. D18, pp. 21925–21932, 1997.
+    DOI: 10.1029/97JD01654.
+[3] J. E. Penner, R. J. Charlson, S.E. Schwartz, J. M. Hales, N. S. Laulainen,
+    L. Travis, R. Leifer, T. Novakov, J. Ogren, L. F. Radke, “Quantifying and
+    Minimizing Uncertainty of Climate Forcing by Anthropogenic Aerosols,”
+    Bulletin of the American Meteorological Society, vol. 75, no. 3,
+    pp. 375–400, 1994.
+    DOI: 10.1175/1520-0477(1994)075<0375:QAMUOC>2.0.CO;2.
+[4] G. Kopp and J. L. Lean, “A new, lower value of total solar irradiance:
+    Evidence and climate significance,” Geophysical Research Letter, vol. 38,
+    no. 1, 2011.
+    DOI: 10.1029/2010GL045777.
+[5] M. Pidwirny, “Introduction to the Oceans,”
+    Fundamentals of Physical Geography, 2nd Edition, (accessed Jan. 25, 2023),
+    2006.
+    URL: http://www.physicalgeography.net/fundamentals/8o.html.
+"""
+import numpy as np
+
+from ..core import UnivariateInput, MultivariateInput
+
+
+DEFAULT_NAME = "sulfur"
+
+DEFAULT_INPUT_MARGINALS_PENNER = [  # From [3] (Table 2)
+    UnivariateInput(
+        name="Q",
+        distribution="lognormal",
+        parameters=[np.log(71.0), np.log(1.15)],
+        description="Source strength of anthropogenic Sulfur [10^12 g/year]",
+    ),
+    UnivariateInput(
+        name="Y",
+        distribution="lognormal",
+        parameters=[np.log(0.5), np.log(1.5)],
+        description="Fraction of SO2 oxidized to SO4(2-) aerosol [-]",
+    ),
+    UnivariateInput(
+        name="L",
+        distribution="lognormal",
+        parameters=[np.log(5.5), np.log(1.5)],
+        description="Average lifetime of atmospheric SO4(2-) [days]",
+    ),
+    UnivariateInput(
+        name="Psi_e",
+        distribution="lognormal",
+        parameters=[np.log(5.0), np.log(1.4)],
+        description="Aerosol mass scattering efficiency [m^2/g]",
+    ),
+    UnivariateInput(
+        name="beta",
+        distribution="lognormal",
+        parameters=[np.log(0.3), np.log(1.3)],
+        description="Fraction of light scattered upward hemisphere [-]",
+    ),
+    UnivariateInput(
+        name="f_Psi_e",
+        distribution="lognormal",
+        parameters=[np.log(1.7), np.log(1.2)],
+        description="Fractional increase in aerosol scattering efficiency "
+        "due to hygroscopic growth [-]",
+    ),
+    UnivariateInput(
+        name="T^2",
+        distribution="lognormal",
+        parameters=[np.log(0.58), np.log(1.4)],
+        description="Square of atmospheric "
+        "transmittance above aerosol layer [-]",
+    ),
+    UnivariateInput(
+        name="(1-Ac)",
+        distribution="lognormal",
+        parameters=[np.log(0.39), np.log(1.1)],
+        description="Fraction of earth not covered by cloud [-]",
+    ),
+    UnivariateInput(
+        name="(1-Rs)^2",
+        distribution="lognormal",
+        parameters=[np.log(0.72), np.log(1.2)],
+        description="Square of surface coalbedo [-]",
+    ),
+]
+
+DEFAULT_INPUTS = {"penner": MultivariateInput(DEFAULT_INPUT_MARGINALS_PENNER)}
+
+DEFAULT_INPUT_SELECTION = "penner"
+
+DEFAULT_PARAMETERS = None
+
+SOLAR_CONSTANT = 1361  # [W/m^2] from [4]
+EARTH_AREA = 5.1e14  # [m^2] from [5]
+DAYS_IN_YEAR = 365  # [days]
+
+
+def evaluate(xx: np.ndarray) -> np.ndarray:
+    """Evaluate the Sulfur model test function on a set of input values.
+
+    References
+    ----------
+    xx : np.ndarray
+        A nine-dimensional input values given by an N-by-9 array
+        where N is the number of input values.
+
+    Returns
+    -------
+    np.ndarray
+        The output of the Sulfur model test function, i.e.,
+        the direct radiative forcing by sulfate aerosols.
+    """
+    # Global input flux of anthropogenic sulfur
+    qq = xx[:, 0] * 1e12
+    # The fraction of SO2 oxidized to SO4(2-)
+    yy = xx[:, 1]
+    # Sulfate lifetime in the atmosphere
+    ll = xx[:, 2]  #
+    # Mass scattering efficiency
+    psi_e = xx[:, 3]  #
+    # Backscattered fraction
+    beta = xx[:, 4]  #
+    # Relative humidity dependent correction factor for the mass scattering eff
+    ff_psi = xx[:, 5]  #
+    # Transmittance of the atmospheric layer above the aerosol layer
+    tt_sq = xx[:, 6]  #
+    # Complement of the fractional cloud cover
+    aa_c_complement = xx[:, 7]
+    # Complement of the mean albedo of the underlying surface
+    co_rr_s_sq = xx[:, 8]
+
+    # Sulfate burden (Eq. (5) in [1], notation from [2])
+    # NOTE: Factor 3.0 due to conversion of mass from S to SO4(2-)
+    # NOTE: Factor 1/365.0 due to average lifetime is given in [days]
+    #       while qq is in [gS / year]
+    sulfate_burden = 3.0 * qq * yy * ll / EARTH_AREA / DAYS_IN_YEAR
+
+    # Loading of sulfate aerosol (Eq. (4) in [1], notation from [2])
+    sulfate_loading = psi_e * ff_psi * sulfate_burden
+
+    # Direct radiative forcing by sulfate aerosols
+    factor_1 = SOLAR_CONSTANT * aa_c_complement * tt_sq * co_rr_s_sq * beta
+    dd_f = -0.5 * factor_1 * sulfate_loading
+
+    return dd_f
---
+++ tests/test_sulfur.py
@@ -0,0 +1,81 @@
+"""
+Test module for the Sulfur model test function.
+
+Notes
+-----
+- The tests defined in this module deals with
+  the correctness of the evaluation.
+"""
+import numpy as np
+
+import uqtestfuns
+from uqtestfuns.test_functions import sulfur as sulfur_mod
+
+
+def _compute_sqrt_geometric_mean_std(marginals):
+    """Compute the sqrt. geom. mean and std. deviation of the Sulfur model."""
+
+    # Compute the mean and std. of the underlying normal distribution
+    mu = 0.0
+    var = 0.0
+    for i in range(9):
+        mu += marginals[i].parameters[0]
+        var += marginals[i].parameters[1] ** 2
+
+    # Plus some additional factors in the Sulfur model formula (only for mu)
+    solar_constant = sulfur_mod.SOLAR_CONSTANT
+    earth_area = sulfur_mod.EARTH_AREA
+    days_in_year = sulfur_mod.DAYS_IN_YEAR
+    mu += (
+        np.log(1e12)
+        + np.log(0.5)
+        + np.log(3)
+        + np.log(solar_constant)
+        - np.log(earth_area)
+        - np.log(days_in_year)
+    )
+
+    # Get the standard deviation
+    sigma = np.sqrt(var)
+
+    return mu, sigma
+
+
+def test_compute_mean():
+    """Test the mean computation as the result is analytical."""
+
+    # Create an instance of Sulfur model
+    my_fun = uqtestfuns.create_from_default("sulfur")
+
+    # Compute mean via Monte Carlo
+    xx = my_fun.input.get_sample(1000000)
+    yy = my_fun(xx)
+
+    mean_mc = np.mean(np.log(-1 * yy))
+
+    # Analytical mean
+    marginals = my_fun.input.marginals
+    mean_ref = _compute_sqrt_geometric_mean_std(marginals)[0]
+
+    # Assertion
+    assert np.allclose(mean_mc, mean_ref, rtol=1e-2)
+
+
+def test_compute_std():
+    """Test the standard deviation computation as the result is analytical."""
+
+    # Create an instance of Sulfur model
+    my_fun = uqtestfuns.create_from_default("sulfur")
+
+    # Compute mean via Monte Carlo
+    xx = my_fun.input.get_sample(1000000)
+    yy = my_fun(xx)
+
+    std_mc = np.std(np.log(-1 * yy))
+
+    # Analytical standard deviation
+    marginals = my_fun.input.marginals
+    std_ref = _compute_sqrt_geometric_mean_std(marginals)[1]
+
+    # Assertion
+    assert np.allclose(std_mc, std_ref, rtol=1e-2)
