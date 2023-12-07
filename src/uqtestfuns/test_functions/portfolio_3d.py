364a7976756fc4f388179f60bf433b47c419bf89
Damar Wicaksono <damar.wicaksono@outlook.com>
2023-12-07 15:43:06 +0100

    Add an implementation of the simple portfolio model.
    The function is a three-dimensional linear function with normally
    distributed input random variables.
    The model is taken from Saltelli et al. (2004).
    
    This commit should resolve Issue #320.

---
+++ src/uqtestfuns/test_functions/portfolio_3d.py
@@ -0,0 +1,104 @@
+"""
+Module with an implementation of the simple 3D portfolio model.
+
+The simple portfolio model is a three-dimensional scalar-valued function.
+The function was first introduced in [1] as an example for illustrating
+different sensitivity measures (i.e., local and hybrid-local).
+
+References
+----------
+
+1. A. Saltelli, S. Tarantola, F. Campolongo, M. Rattom, "Sensitivity analysis
+   in practice: a guide to assessing scientific models," Hoboken, NJ: Wiley,
+   2004.
+"""
+import numpy as np
+
+from typing import Tuple
+
+from ..core.prob_input.input_spec import UnivDistSpec, ProbInputSpecFixDim
+from ..core.uqtestfun_abc import UQTestFunABC
+
+__all__ = ["Portfolio3D"]
+
+
+INPUT_MARGINALS_SALTELLI2004 = [
+    UnivDistSpec(
+        name="Ps",
+        distribution="normal",
+        parameters=[0.0, 4.0],
+        description="Hedged portfolio 's' [\N{euro sign}]",
+    ),
+    UnivDistSpec(
+        name="Pt",
+        distribution="normal",
+        parameters=[0.0, 2.0],
+        description="Hedged portfolio 't' [\N{euro sign}]",
+    ),
+    UnivDistSpec(
+        name="Pj",
+        distribution="normal",
+        parameters=[0.0, 1.0],
+        description="Hedged portfolio 'j' [\N{euro sign}]",
+    ),
+]
+
+AVAILABLE_INPUT_SPECS = {
+    "Saltelli2004": ProbInputSpecFixDim(
+        name="Portfolio3D-Saltelli2004",
+        description=(
+            "Probabilistic input model for the simple portfolio model "
+            "from Saltelli et al. (2004)."
+        ),
+        marginals=INPUT_MARGINALS_SALTELLI2004,
+        copulas=None,
+    ),
+}
+
+AVAILABLE_PARAMETERS = {
+    "Saltelli2004a": (100.0, 500.0, 1000.0),  # from [1]
+    "Saltelli2004b": (300.0, 300.0, 300.0),  # from [1]
+    "Saltelli2004c": (500.0, 400.0, 100.0),  # from [1]
+}
+
+DEFAULT_PARAMETERS_SELECTION = "Saltelli2004a"
+
+
+def evaluate(xx: np.ndarray, parameters: Tuple[float, float, float]):
+    """Evaluate the simple portfolio model on a set of input values.
+
+    Parameters
+    ----------
+    xx : np.ndarray
+        Three-Dimensional input values given by an ``(N, 3)`` array where
+        ``N`` is the number of input values.
+    parameters : Tuple[float, float, float]
+        Tuple of three values as the parameters of the function (i.e.,
+        the quantities per hedged portfolio).
+
+    Returns
+    -------
+    np.ndarray
+        The output of the simple portfolio model on the given input values.
+        The output is a one-dimensional array of length ``N``.
+    """
+    # Read the parameters
+    p = np.array(parameters)
+
+    # Compute the simple portfolio model
+    yy = np.sum(p * xx, axis=1)
+
+    return yy
+
+
+class Portfolio3D(UQTestFunABC):
+    """An implementation of the simple portfolio model test function."""
+
+    _tags = ["sensitivity"]
+    _description = "Simple portfolio model from Saltelli et al. (2004)"
+    _available_inputs = AVAILABLE_INPUT_SPECS
+    _available_parameters = AVAILABLE_PARAMETERS
+    _default_parameters = DEFAULT_PARAMETERS_SELECTION
+    _default_spatial_dimension = 3
+
+    eval_ = staticmethod(evaluate)
---
+++ tests/builtin_test_functions/test_portfolio_3d.py
@@ -0,0 +1,77 @@
+"""
+Test module for the simple portfolio model.
+
+Notes
+-----
+- The tests defined in this module deals with
+  the correctness of the evaluation.
+"""
+import numpy as np
+import pytest
+
+from uqtestfuns import Portfolio3D
+import uqtestfuns.test_functions.portfolio_3d as portfolio3d_mod
+
+# Test for different sets of parameters of the simple portfolio model
+available_parameters = list(Portfolio3D.available_parameters.keys())
+
+
+@pytest.fixture(params=available_parameters)
+def portfolio3d_fun(request):
+    portfolio3d = Portfolio3D(parameters_selection=request.param)
+
+    return portfolio3d
+
+
+def test_compute_mean(portfolio3d_fun):
+    """Test the analytical mean."""
+
+    # Analytical mean
+    mean_inputs = np.array(
+        [x.parameters[0] for x in portfolio3d_fun.prob_input.marginals]
+    )
+    parameters = np.array(portfolio3d_fun.parameters)
+    mean_ref = mean_inputs @ parameters
+
+    # Assertion
+    assert np.allclose(mean_ref, 0)
+
+
+def test_compute_variance(portfolio3d_fun):
+    """Test the analytical variance."""
+
+    # Compute variance via Monte Carlo
+    xx = portfolio3d_fun.prob_input.get_sample(10000000)
+    yy = portfolio3d_fun(xx)
+
+    var_mc = np.var(yy)
+
+    # Analytical variance
+    std_inputs = np.array(
+        [x.parameters[1] for x in portfolio3d_fun.prob_input.marginals]
+    )
+    parameters = np.array(portfolio3d_fun.parameters)
+    var_ref = parameters**2 @ std_inputs**2
+
+    # Assertion
+    assert np.allclose(var_mc, var_ref, rtol=1e-1)
+
+
+@pytest.mark.parametrize("param_selection", available_parameters)
+def test_different_parameters(param_selection):
+    """Test selecting different built-in parameters."""
+
+    # Create an instance of Ishigami function with a specified param. selection
+    my_testfun = Portfolio3D(parameters_selection=param_selection)
+
+    # Assertion
+    assert (
+        my_testfun.parameters
+        == portfolio3d_mod.AVAILABLE_PARAMETERS[param_selection]
+    )
+
+
+def test_wrong_param_selection():
+    """Test a wrong selection of the parameters."""
+    with pytest.raises(KeyError):
+        Portfolio3D(parameters_selection="marelli1")
