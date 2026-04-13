4fe2f51719bcf39c7d9c0ac70dadf43839efc18e
Damar Wicaksono <damar.wicaksono@outlook.com>
2026-04-13 22:13:13 +0200

    refactor(marginal): Rename `transform_sample` to `transform_to`
    Renamed `transform_sample` method to `transform_to`
    for better clarity and consistency.
    Updated references in tests and improved related docstrings.
    
    Refs: #452, #459

--- tests/core/prob_input/test_univariate_input.py
+++ tests/core/prob_input/test_univariate_input.py
@@ -186,7 +186,7 @@ def test_transform_sample() -> None:
         name=name_2, distribution=distribution_2, parameters=parameters_2
     )
 
-    xx_trans = my_univariate_input_1.transform_sample(
+    xx_trans = my_univariate_input_1.transform_to(
         xx, my_univariate_input_2
     )
 
@@ -209,7 +209,7 @@ def test_failed_transform_sample() -> None:
     xx = my_univariate_input.get_sample(sample_size)
 
     with pytest.raises(TypeError):
-        my_univariate_input.transform_sample(xx, [])  # type: ignore
+        my_univariate_input.transform_to(xx, [])  # type: ignore
 
 
 def test_cdf_monotonously_increasing(univariate_input: Any) -> None:
